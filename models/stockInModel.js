895112d3a1bb927ae2da29c6a523cd00e9c6b1c6
Vivek Sharma <vivek@example.com>
2025-08-29 17:58:19 +0530

    Added new features and fixed bugs

--- models/stockInModel.js
+++ models/stockInModel.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
 const stockInSchema = new mongoose.Schema({
-    product_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-    quantity: { type: Number },
-    unit:{type:String, enum:[ 'kg', 'liter', 'piece', 'packet', 'box' ]},
-    product_quantity: { type: Number, required: true },
-    date: { type: Date, default: Date.now },
-    note: { type: String },
+  product_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+  quantity: { type: Number, default: 0 }, // agar na bhejo to default 0
+  unit: { 
+    type: String, 
+    enum: ['kg', 'liter', 'piece', 'packet', 'box', ''], // 👈 empty string allowed
+    default: '' 
+  },
+  product_quantity: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+  note: { type: String, default: "" }
 });
 
 const StockIn = mongoose.model("StockIn", stockInSchema);
--- models/stockOutModel.js
+++ models/stockOutModel.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
 const stockOutSchema = new mongoose.Schema({
-    product_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-    quantity: { type: Number, },
-    unit:{type:String, enum:[ 'kg', 'liter', 'piece', 'packet', 'box' ]},
-    product_quantity: { type: Number, required: true },
-    date: { type: Date, default: Date.now },
-    note: { type: String },
+  product_id: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+  quantity: { type: Number, default: 0 }, // agar na bheje to 0
+  unit: { 
+    type: String, 
+    enum: ['kg', 'liter', 'piece', 'packet', 'box', ''], // 👈 empty string allowed
+    default: '' 
+  },
+  product_quantity: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+  note: { type: String, default: "" }
 });
 
 const StockOut = mongoose.model("StockOut", stockOutSchema);
